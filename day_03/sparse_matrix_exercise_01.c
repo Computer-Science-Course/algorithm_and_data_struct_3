dee1f9e4b0ca1a730157b851f1c8fb3413eced1d
Mateus Santos <a.mateussm1.5@gmail.com>
2023-01-19 21:01:40 -0300

    :wrench: | day 03

---
+++ day_03/sparse_matrix_exercise_01.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <malloc.h>
+
+typedef struct tempNo
+{
+    float valor;
+    int coluna;
+    struct tempNo *prox;
+} NO;
+
+typedef NO *PONT;
+typedef struct
+{
+    PONT *A;
+    int linhas;
+    int colunas;
+} MATRIZ;
+
+// An empty matrix
+void inicializarMatriz(MATRIZ *m, int lin, int col)
+{
+    int i;
+    m->linhas = lin;
+    m->colunas = col;
+    m->A = (PONT *)malloc(lin * sizeof(PONT));
+
+    for (i = 0; i < lin; i++)
+        m->A[i] = NULL;
+}
+
+int AtribuiMatriz(MATRIZ *m, int lin, int col,
+                   float val)
+{
+    // Invalid position
+    if (lin < 0 || lin >= m->linhas ||
+        col < 0 || col >= m->colunas)
+        return 0;
+
+    PONT ant = NULL;
+    PONT atual = m->A[lin];
+    while (atual != NULL && atual->coluna < col)
+    {
+        ant = atual;
+        atual = atual->prox;
+    }
+    if (atual != NULL && atual->coluna == col)
+    {
+        if (val == 0)
+        {
+            if (ant == NULL)
+                m->A[lin] = atual->prox;
+            else
+                ant->prox = atual->prox;
+            free(atual);
+        }
+        else
+            atual->valor = val;
+    }
+    else if (val != 0)
+    {
+        PONT novo = (PONT)malloc(sizeof(NO));
+        novo->coluna = col;
+        novo->valor = val;
+        novo->prox = atual;
+        if (ant == NULL)
+            m->A[lin] = novo;
+        else
+            ant->prox = novo;
+    }
+    return 1;
+}
+
+void main(){
+
+}
